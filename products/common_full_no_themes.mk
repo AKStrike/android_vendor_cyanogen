c6679604666737328490c0c8693535a355904fe0
eagleeyetom <tomekondrat@wp.pl>
2012-09-28 10:43:13 +0200

    overlay and default sounds
    Change-Id: I2dd933ef70eb1d72c88eddceeaa754be729aa203

--- products/common_full_no_themes.mk
+++ products/common_full_no_themes.mk
@@ -13,6 +13,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/dictionaries
 
 # Default Ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Playa.ogg \
-    ro.config.notification_sound=regulus.ogg \
-    ro.config.alarm_alert=Alarm_Beep_03.ogg
+    ro.config.ringtone=CyanTone.ogg \
+    ro.config.notification_sound=CyanMessage.ogg \
+    ro.config.alarm_alert=CyanAlarm.ogg
